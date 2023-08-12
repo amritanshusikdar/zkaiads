b20d8e5fc8c3c75331d9ec79fa1b443a478e2da8
ksavul <55496444+ksavul@users.noreply.github.com>
2023-08-12 13:17:42 +0200

    adding solidity files

---
+++ solidity/scripts/deploy-counter.js
@@ -0,0 +1,14 @@
+const hre = require("hardhat");
+
+async function main() {
+  const deployedContract = await hre.ethers.deployContract("Counter");
+  await deployedContract.waitForDeployment();
+  console.log(
+    `Counter contract deployed to https://explorer.public.zkevm-test.net/address/${deployedContract.target}`
+  );
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
