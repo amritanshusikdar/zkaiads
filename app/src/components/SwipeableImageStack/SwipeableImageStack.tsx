9eb3f68f80453f61c7b4e9268263295483176e76
Amritanshu Sikdar <amritanshu.sikdar@gmail.com>
2023-08-12 14:23:23 +0200

    add preliminary card swiper

--- app/src/components/SwipeableImageStack/SwipeableImageStack.tsx
+++ app/src/components/SwipeableImageStack/SwipeableImageStack.tsx
@@ -1,18 +1,73 @@
 import './SwipeableImageStack.css';
 import {IconButton} from "../IconButton/IconButton";
 import {checkmark, close} from "ionicons/icons";
-import {IonImg} from "@ionic/react";
+import {
+    CreateAnimation,
+    createGesture,
+    Gesture,
+    GestureDetail,
+    IonCard,
+    IonImg,
+    useIonViewDidEnter
+} from "@ionic/react";
+import {useRef} from "react";
 
-interface ContainerProps { }
+interface SwipeableImageStackProps {
+}
 
-export const SwipeableImageStack: React.FC<ContainerProps> = () => {
-    return (
-        <div id="container">
-            <div style={{padding: 20}}>
-                <IonImg src={"assets/ironman.jpg"} alt={"Iron Man"} />
-            </div>
-            <IconButton icon={close} text={"Nope"} />
-            <IconButton icon={checkmark} text={"Yep"} />
+export const SwipeableImageStack: React.FC<SwipeableImageStackProps> = () => {
+    return <>
+        <div style={{position: "relative"}}>
+            <SwipeableImage />
+            <SwipeableImage />
         </div>
+        <div>
+            <IconButton icon={close} text={"Nope"}/>
+            <IconButton icon={checkmark} text={"Yep"}/>
+        </div>
+    </>
+}
+
+const SwipeableImage: React.FC<SwipeableImageStackProps> = () => {
+    const windowWidth = window.innerWidth;
+    const cardRef = useRef<HTMLIonCardElement | null>(null);
+
+    useIonViewDidEnter(() => {
+        const cardRefStyle = cardRef.current?.style;
+        const gesture: Gesture = createGesture({
+            el: cardRef.current!,
+            gestureName: "card-swipe",
+            onStart: (detail: GestureDetail) => {
+                cardRefStyle!.transition = "none";
+            },
+            onMove: (detail: GestureDetail) => {
+                cardRefStyle!.transform = `translateX(${detail.deltaX}px) rotate(${
+                    detail.deltaX / 20
+                }deg)`;
+            },
+            onEnd: (detail: GestureDetail) => {
+                cardRefStyle!.transition = '0.3s ease-out';
+
+                if (detail.deltaX > windowWidth / 2) {
+                    cardRefStyle!.transform = `translateX(${windowWidth * 1.5}px)`;
+                } else if (detail.deltaX < -windowWidth / 2) {
+                    cardRefStyle!.transform = `translateX(-${windowWidth * 1.5}px)`;
+                } else {
+                    cardRefStyle!.transform = '';
+                }
+            },
+        });
+        gesture.enable();
+    })
+
+    return (
+        <IonCard style={{
+            padding: 20,
+            position: "absolute",
+            top: 0,
+            left: 0,
+        }} ref={cardRef}>
+            <IonImg src={"assets/ironman.jpg"} alt={"Iron Man"}/>
+        </IonCard>
     );
 };
