7674e56bfdf99a0703789301c718f8964740f4b3
Amritanshu Sikdar <amritanshu.sikdar@gmail.com>
2023-08-12 16:24:22 +0200

    init express project

---
+++ stream/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+
+const adsList = {
+  images: [
+    {
+      path: "assets/sale1.png",
+      alt: "sale1",
+    },
+    {
+      path: "assets/sale2.png",
+      alt: "sale2",
+    },
+    {
+      path: "assets/sale3.png",
+      alt: "sale3",
+    },
+    {
+      path: "assets/sale4.png",
+      alt: "sale4",
+    },
+    {
+      path: "assets/sale5.png",
+      alt: "sale5",
+    },
+  ],
+};
+
+app.get("/", (request, response) => {
+  response.send(adsList);
+});
+
+app.listen(3000, () => {
+  console.log("Listen on the port 3000...");
+});
