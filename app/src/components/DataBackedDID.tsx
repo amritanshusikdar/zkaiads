e6b9ae3b0deca570d57e4de08a3c7e1fc53c05e3
Tobias Leinß <tobias.leinss@mway.io>
2023-08-12 11:20:43 +0200

    Add basic ui components with text content

---
+++ app/src/components/DataBackedDID.tsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react"
+import { ellipseOutline } from "ionicons/icons"
+import { IonContent, IonPage, IonIcon } from "@ionic/react"
+import { Swiper, SwiperSlide } from "swiper/react"
+
+import "swiper/css"
+import "@ionic/react/css/ionic-swiper.css"
+
+enum PageState {
+  "a",
+  "b",
+  "c",
+}
+
+export default function DataBackedDID() {
+  const [pageState, setPageState] = useState(PageState.a)
+
+  const onStart = () => {
+    console.log("NOT IMPLEMENTED")
+  }
+
+  const renderContent = () => {
+    let content = null
+
+    switch (pageState) {
+      case PageState.a:
+        content = (
+          <p>
+            Imagine you have a special digital name, created by mixing little
+            bits from your online activities.
+          </p>
+        )
+        break
+
+      case PageState.b:
+        content = (
+          <p>
+            This name is unique just like your fingerprint. That's your DDID,
+            and it's your online identity without revealing personal secrets.
+          </p>
+        )
+        break
+
+      case PageState.c:
+        content = (
+          <div>
+            <p>
+              Imagine a sybil resistant DID without the need to scan your
+              eyeball!
+            </p>
+
+            <button onClick={onStart}>Start</button>
+          </div>
+        )
+    }
+
+    return content
+  }
+
+  return (
+    <IonPage>
+      <IonContent>
+        <h1>Data-backed DID</h1>
+
+        <Swiper>
+          <SwiperSlide>{renderContent()}</SwiperSlide>
+          <SwiperSlide>{renderContent()}</SwiperSlide>
+          <SwiperSlide>{renderContent()}</SwiperSlide>
+        </Swiper>
+      </IonContent>
+    </IonPage>
+  )
+}
